5b5867f3025a286c3ffdfdaaddeb77d98a3fd853
CL <cl.czayas@gmail.com>
2022-11-08 15:06:05 -0400

    Fetched first part--translating city into coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
-const requestURL = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4ab55ec96aeb47baa2457edf5e49043e"
-const geoURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=4ab55ec96aeb47baa2457edf5e49043e"
+const searchbox = document.getElementById('city');
+const APIkey = "4ab55ec96aeb47baa2457edf5e49043e";
+
+$("#search").click(function() {
+    var cityInput = searchbox.value;
+    console.log(cityInput);
+
+    const geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + APIkey;
+
+    //Fetch Method
+    fetch(geoURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    for (i in data) {
+    console.log(data[i].lat, data[i].lon);
+    }
+})
+})
